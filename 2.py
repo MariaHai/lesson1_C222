422d496f166a8155465f7e85300885bf33f8761d
MariaHai <Maria12345>
2022-05-19 19:21:28 +0300

    HW3

--- 2.py
+++ 2.py
@@ -1,14 +1,39 @@
-x = 10
-class Looker:
-    # x = 15
-    print(x)
-    def func_1(self):
-        # x = 20
-        print(x)
-        def func_2():
-            # x = 30
-            print(x)
-        func_2()
-some_object
+class Student:
+    def __init__(self, name=None, height=160):
+        self.height = height
+        Student.amount_of_students+=1
+    def grow(self, height=1):
+        self.height+=height
+nick = Student()
+kate = Student(height=170)
+nick.grow(height=15)
+print(kate.height)
+print(nick.height)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x = 10
+# class Looker:
+#     # x = 15
+#     print(x)
+#     def func_1(self):
+#         # x = 20
+#         print(x)
+#         def func_2():
+#             # x = 30
+#             print(x)
+#         func_2()
+# some_object = Looker()
+# some_object.func_1()
 
 
---
+++ hw3.py
@@ -0,0 +1,19 @@
+class cat:
+    hp = 100
+    eat = 100
+    name = ""
+    def __int__(self,n):
+        self.name = n
+        print(self.name,"hello")
+        self.live()
+        self.live()
+        self.eating()
+    def eating(self):
+        self.eat += 10
+        print(self.eat)
+    def live(self):
+        self.eat -= 5
+        print(self.eat)
+    def damage(self):
+        self.hp -= 25
+weywuxian = ("Wey Wuxin")
